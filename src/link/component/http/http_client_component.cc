deeab6148dcf290737681c9785f070eb04b638e8
ISSuh <yoyus5@naver.com>
2022-06-30 23:32:08 +0900

    [dev] try implement http client

--- src/link/component/http/http_client_component.cc
+++ src/link/component/http/http_client_component.cc
@@ -7,17 +7,142 @@
 #include "link/component/http/http_client_component.h"
 
 #include "link/base/logging.h"
+#include "link/net/base/uri.h"
+#include "link/net/http/response_parser.h"
 
 namespace nlink {
 namespace component {
 
-HttpClientComponent::HttpClientComponent() {
+HttpClientComponent::HttpClientComponent(
+  const std::string& host, uint16_t port) {
 }
 
 HttpClientComponent::~HttpClientComponent() {
 }
 
+void HttpClientComponent::Get(
+  const std::string& path, RequestHanelder handler) {
+  const std::string scheme = is_https ? "https" : "http";
+  const std::string host = url_ + std::to_string(port_);
+  net::Uri uri(scheme, host, path);
+  net::http::Request request(net::http::Method::GET, uri);
+
+  request.Serialize();
+
+}
+
+void HttpClientComponent::Get(
+  const std::string& path,
+  const net::http::HttpHeader& header,
+  RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Post(
+  const std::string& path,
+  const std::string& content_type,
+  const std::string& body,
+  RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Post(
+  const std::string& path,
+  const net::http::HttpHeader& header,
+  const std::string& content_type,
+  const std::string& body,
+  RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Put(
+  const std::string& path, RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Put(
+  const std::string& path,
+  const net::http::HttpHeader& header,
+  RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Delete(
+  const std::string& path, RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Delete(
+  const std::string& path,
+  const net::http::HttpHeader& header,
+  RequestHanelder handler) {
+
+}
+
+void HttpClientComponent::Options(
+  const std::string& path, RequestHanelder handler) {
+  
+}
+
+void HttpClientComponent::Options(
+  const std::string& path,
+  const net::http::HttpHeader& header,
+  RequestHanelder handler) {
+  
+}
+
+void HttpClientComponent::Fetch(
+  net::http::Method method,
+  const std::string& path,
+  RequestHanelder handler,
+  const net::http::HttpHeader& header,
+  const std::string& content_type,
+  const std::string& body) {
+  switch (method) {
+  case net::http::Method::GET:
+    Get(path, header, handler);
+    break;
+  case net::http::Method::POST:
+    Post(path, header, content_type, body, handler);
+    break;
+  case net::http::Method::PUT:
+    Put(path, header, handler);
+    break;
+  case net::http::Method::DELETE:
+    Delete(path, header, handler);
+    break;
+  default:
+    LOG(WARNING) << "[HttpClientComponent::Fetch] unsupport method. "
+                 << MethodToString(method);
+    break;
+  }
+}
+
 base::EventChannel* HttpClientComponent::GetEventChannel() {
+}
+
+void HttpClientComponent::InternalConnectHandler(
+  std::shared_ptr<io::Session> session) {
+  if (session == nullptr) {
+    return;
+  }
+}
+
+void HttpClientComponent::InternalCloseHandler(
+  std::shared_ptr<io::Session> session) {
+  if (session == nullptr) {
+    return;
+  }
+}
+
+void HttpClientComponent::InternalReadHandler(
+  const base::Buffer& buffer, std::shared_ptr<io::Session> session) {
+  net::http::Response response = net::http::ResponseParser::Parse(buffer);
+
+
+}
+
+void HttpClientComponent::InternalWriteHandler(size_t length) {
 }
 
 }  // namespace component
--- src/link/component/http/http_client_component.h
+++ src/link/component/http/http_client_component.h
@@ -7,7 +7,16 @@
 #ifndef LINK_COMPONENT_HTTP_HTTP_CLIENT_COMPONENT_H_
 #define LINK_COMPONENT_HTTP_HTTP_CLIENT_COMPONENT_H_
 
+#include <string>
+#include <memory>
+
+#include "link/base/callback/callback.h"
 #include "link/component/http/http_component.h"
+#include "link/net/http/method.h"
+#include "link/net/http/header.h"
+#include "link/net/http/request.h"
+#include "link/net/http/response.h"
+#include "link/component/ipc/socket/tcp_client_component.h"
 
 namespace nlink {
 namespace component {
@@ -16,13 +25,71 @@ class HttpComponent;
 
 class HttpClientComponent : public HttpComponent {
  public:
+  using RequestHanelder = base::Callback<void(const net::http::Response&)>;
+
+  void Get(const std::string& path, RequestHanelder handler);
+  void Get(
+    const std::string& path,
+    const net::http::HttpHeader& header,
+    RequestHanelder handler);
+
+  void Post(
+    const std::string& path,
+    const std::string& content_type,
+    const std::string& body,
+    RequestHanelder handler);
+  void Post(
+    const std::string& path,
+    const net::http::HttpHeader& header,
+    const std::string& content_type,
+    const std::string& body,
+    RequestHanelder handler);
+
+  void Put(const std::string& path, RequestHanelder handler);
+  void Put(
+    const std::string& path,
+    const net::http::HttpHeader& header,
+    RequestHanelder handler);
+
+  void Delete(const std::string& path, RequestHanelder handler);
+  void Delete(
+    const std::string& path,
+    const net::http::HttpHeader& header,
+    RequestHanelder handler);
+
+  void Options(const std::string& path, RequestHanelder handler);
+  void Options(
+    const std::string& path,
+    const net::http::HttpHeader& header,
+    RequestHanelder handler);
+
+  void Fetch(
+    net::http::Method method,
+    const std::string& path,
+    RequestHanelder handler,
+    const net::http::HttpHeader& header = net::http::HttpHeader(),
+    const std::string& content_type = "",
+    const std::string& body = "");
 
  private:
-  base::EventChannel* GetEventChannel() override;
+  HttpClientComponent(const std::string& host, uint16_t port);
+  virtual ~HttpClientComponent();
 
   // LinkComponent
-  HttpClientComponent();
-  virtual ~HttpClientComponent();
+  base::EventChannel* GetEventChannel() override;
+
+  void InternalConnectHandler(std::shared_ptr<io::Session> session);
+  void InternalCloseHandler(std::shared_ptr<io::Session> session);
+  void InternalReadHandler(
+    const base::Buffer& buffer, std::shared_ptr<io::Session> session);
+  void InternalWriteHandler(size_t length);
+
+  bool is_https;
+  std::string url_;
+  uint16_t port_;
+
+
+  std::unique_ptr<io::Client> client_;
 
   DISAALOW_COPY_AND_ASSIGN(HttpClientComponent);
 };
